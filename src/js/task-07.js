4be325ca2c2298d692b6dd72ad6fda0e3553cee9
Kateryna Ostrynska <kateriinag@gmail.com>
2022-09-12 17:21:17 +0300

    Update task-07.js

---
+++ src/js/task-07.js
@@ -0,0 +1,12 @@
+const refs = {
+    range: document.querySelector('#font-size-control'),
+    text: document.querySelector('#text'),
+};
+
+refs.range.value = 16;
+refs.range.addEventListener('change', onInputChange);
+
+function onInputChange(event)
+{
+    refs.text.style.fontSize = refs.range.value + 'px';
+};
