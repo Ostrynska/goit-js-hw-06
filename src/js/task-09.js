b50321e8bdd06e8226c6755dae56d5d260633e3e
Kateryna Ostrynska <kateriinag@gmail.com>
2022-09-12 20:32:04 +0300

    add task 9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,19 @@
-function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+const refs = {
+  color: document.querySelector('.color'),
+  btn: document.querySelector('.change-color'),
+  body: document.querySelector('body'),
+};
+
+refs.btn.addEventListener('click', getRandomHexColor);
+
+function getRandomHexColor(event)
+{
+  const result = `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+  
+  refs.color.textContent = result;
+  refs.body.style.backgroundColor = result;
+
+  return result;
 }
