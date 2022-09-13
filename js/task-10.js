c25430f5fa18ead7a442afc7b56c5c39348f9272
Kateryna Ostrynska <kateriinag@gmail.com>
2022-09-13 12:07:51 +0300

    Update task-10.js

--- js/task-10.js
+++ js/task-10.js
@@ -3,31 +3,32 @@ const btnCreate = document.querySelector('[data-create]');
 const btnDestroy = document.querySelector('[data-destroy]');
 const boxesContainer = document.querySelector('#boxes');
 
+
 let amount = 0;
 
 inputForm.addEventListener('input', event => amount = event.currentTarget.value);
 btnCreate.addEventListener('click', () => createBoxes(amount));
-btnDestroy.addEventListener('click', () => boxesContainer.innerHTML = '');
+btnDestroy.addEventListener('click', () =>
+{
+  boxesContainer.innerHTML = '';
+  inputForm.value = "";
+});
 
-
-let boxSize = 30;
 
 function createBoxes(amount)
 {
-
-  const box = amount.map(item =>
-  {
+  for (let i = 0, boxsize = 30; i < amount; i += 1, boxsize +=10) {
       const newBox = document.createElement('div');
       newBox.classList.add('boxes-item');
+
+      newBox.style.width = `${boxsize}` + 'px';
+      newBox.style.height = `${boxsize}` + 'px';
+      newBox.style.marginBottom = 7 + 'px';
+
       newBox.style.backgroundColor = getRandomHexColor();
-      newBox.style.width = `${size}`;
-      newBox.style.height = `${size}`;
       boxesContainer.appendChild(newBox);
-
-      return newBox;
-    });
+  }
 };
-
 
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
