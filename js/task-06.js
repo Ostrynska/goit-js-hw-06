bf3fe45b872ac9e42023ccad2a4f87943d86e3f6
Kateryna Ostrynska <kateriinag@gmail.com>
2022-09-13 22:48:11 +0300

    update

--- js/task-06.js
+++ js/task-06.js
@@ -3,13 +3,12 @@ const dataLength = input.getAttribute('data-length');
 
 input.addEventListener('blur', onInputBlur);
 
-function onInputBlur()
-{ 
-    if (dataLength < input.value.length || dataLength > input.value.length) {
-        input.classList.remove("valid");
-        input.classList.add("invalid");
-    } else {
-        input.classList.remove("invalid");
-        input.classList.add("valid");
-    }
-};
+function onInputBlur() {
+  if (Number(dataLength) === input.value.length) {
+    input.classList.add('valid');
+    input.classList.remove('invalid');
+  } else {
+    input.classList.remove('valid');
+    input.classList.add('invalid');
+  }
+}
--- js/task-07.js
+++ js/task-07.js
@@ -1,12 +1,11 @@
 const refs = {
-    range: document.querySelector('#font-size-control'),
-    text: document.querySelector('#text'),
+  range: document.querySelector('#font-size-control'),
+  text: document.querySelector('#text'),
 };
 
 refs.range.value = 16;
 refs.range.addEventListener('change', onInputChange);
 
-function onInputChange(event)
-{
-    refs.text.style.fontSize = refs.range.value + 'px';
-};
+function onInputChange(event) {
+  refs.text.style.fontSize = refs.range.value + 'px';
+}
